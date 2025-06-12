ad6c4b6cbc435b878e91bae22f3c62d8e1c89090
mike heintzman <mikeheintzman@gmail.com>
2025-06-12 00:02:37 +0000

    feat: implement mouse and keyboard input using java.awt.Robot
    Co-authored-by: Genie <genie@cosine.sh>

--- src/main/java/com/osrsbot/api/modules/InputApi.java
+++ src/main/java/com/osrsbot/api/modules/InputApi.java
@@ -7,23 +7,64 @@ import java.awt.*;
  * Input API for simulating mouse/keyboard input.
  */
 public class InputApi {
+    private static java.awt.Robot robot = createRobot();
+
+    private static java.awt.Robot createRobot() {
+        try {
+            return new java.awt.Robot();
+        } catch (Exception e) {
+            DebugManager.logException(e);
+            return null;
+        }
+    }
+
     public void click(int x, int y) {
         DebugManager.logApiCall("InputApi.click(" + x + ", " + y + ")");
-        // TODO: Inject mouse event (use java.awt.Robot or native code)
+        moveMouse(x, y);
+        if (robot != null) {
+            robot.mousePress(java.awt.event.InputEvent.BUTTON1_DOWN_MASK);
+            robot.delay(30);
+            robot.mouseRelease(java.awt.event.InputEvent.BUTTON1_DOWN_MASK);
+        }
     }
 
     public void rightClick(int x, int y) {
         DebugManager.logApiCall("InputApi.rightClick(" + x + ", " + y + ")");
-        // TODO: Inject right mouse click
+        moveMouse(x, y);
+        if (robot != null) {
+            robot.mousePress(java.awt.event.InputEvent.BUTTON3_DOWN_MASK);
+            robot.delay(30);
+            robot.mouseRelease(java.awt.event.InputEvent.BUTTON3_DOWN_MASK);
+        }
     }
 
     public void moveMouse(int x, int y) {
         DebugManager.logApiCall("InputApi.moveMouse(" + x + ", " + y + ")");
-        // TODO: Move mouse cursor
+        if (robot != null) {
+            robot.mouseMove(x, y);
+        }
     }
 
     public void type(String text) {
         DebugManager.logApiCall("InputApi.type(" + text + ")");
-        // TODO: Inject keyboard input
+        if (robot != null) {
+            for (char c : text.toCharArray()) {
+                try {
+                    boolean upper = Character.isUpperCase(c);
+                    int keyCode = java.awt.event.KeyEvent.getExtendedKeyCodeForChar(c);
+                    if (upper) {
+                        robot.keyPress(java.awt.event.KeyEvent.VK_SHIFT);
+                    }
+                    robot.keyPress(keyCode);
+                    robot.keyRelease(keyCode);
+                    if (upper) {
+                        robot.keyRelease(java.awt.event.KeyEvent.VK_SHIFT);
+                    }
+                    robot.delay(10);
+                } catch (Exception e) {
+                    DebugManager.logException(e);
+                }
+            }
+        }
     }
 }
